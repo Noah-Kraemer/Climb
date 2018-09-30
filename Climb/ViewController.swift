07ce06b12e3cde4de054c1940cbfea3a02ccc18a
Noah Kraemer <s4291502@student.uq.edu.au>
2018-09-30 11:22:20 +1000

    Initial Commit

---
+++ Climb/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Climb
+//
+//  Created by Noah Kraemer on 30/9/18.
+//  Copyright © 2018 Noah Kraemer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
