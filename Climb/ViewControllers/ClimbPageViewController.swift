68b81ab628a1188bd7df1da5de654024108c9956
Noah Kraemer <s4291502@student.uq.edu.au>
2018-10-11 19:14:04 +1000

    Update icon names.

--- Climb/ViewControllers/ClimbPageViewController.swift
+++ Climb/ViewControllers/ClimbPageViewController.swift
@@ -48,7 +48,7 @@ class ClimbPageViewController: UIPageViewController, UIPageViewControllerDataSou
         newClimb.setValue("67m", forKey: "length")
         newClimb.setValue("The Free Route - Totem Pole", forKey: "name")
         newClimb.setValue("tote.jpg", forKey: "imageName")
-        newClimb.setValue("Sport", forKey: "style")
+        newClimb.setValue("sport", forKey: "style")
         
         let newClimb2 = NSManagedObject(entity: entity!, insertInto: context)
         newClimb2.setValue("25m 24. Crank through the overhang at the base of the NE arete. Continue up the arete and the wall to its left. \n\n10m 20. Blast straight up from the belay ledge then trend right via the last U to finish on the right side of the arete. No natural gear required on either pitch.", forKey: "details")
@@ -57,7 +57,7 @@ class ClimbPageViewController: UIPageViewController, UIPageViewControllerDataSou
         newClimb2.setValue("35m", forKey: "length")
         newClimb2.setValue("Ancient Astronaught - Moai", forKey: "name")
         newClimb2.setValue("moai.jpg", forKey: "imageName")
-        newClimb2.setValue("Mixed", forKey: "style")
+        newClimb2.setValue("mixed", forKey: "style")
         
         let newClimb3 = NSManagedObject(entity: entity!, insertInto: context)
         newClimb3.setValue("Rap off the rings as for the Totem pole, jump in and swim right to an easy ledge just behind the Totem pole.\n\n30m Up chimney / crack\n\n30m More chimney / crack, ending on a good ledge with 2 rings to tyrolean back to the mainland roughly level with the top of the tote.\n\n30m through chimney past big chockstone (on descent it's easy to get your rope stuck on this), then up through looser rock into the sunshine to a rusty anchor.\n\n20m Up sunny face veering a little left at the top to another rusty anchor then scramble to the real summit.", forKey: "details")
@@ -66,7 +66,7 @@ class ClimbPageViewController: UIPageViewController, UIPageViewControllerDataSou
         newClimb3.setValue("110m", forKey: "length")
         newClimb3.setValue("Normal Route - Candlestick", forKey: "name")
         newClimb3.setValue("candlestick.jpg", forKey: "imageName")
-        newClimb3.setValue("Trad", forKey: "style")
+        newClimb3.setValue("trad", forKey: "style")
         
         do {
             try context.save()
